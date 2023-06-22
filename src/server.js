ce4216e9dffb05377511001784709cd2d744ef95
Franjhe <franjhely.andre13@gmail.com>
2023-06-22 15:07:27 -0400

    add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
+import cors from 'cors';
 import v1AuthRouter from './v1/routes/authRoutes.js';
 
 const app = express(); 
+
 dotenv;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", v1AuthRouter);
