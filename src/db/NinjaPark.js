90f68b97b10d483c5fb0939e93ac360e7cfe7308
Jhon Alen <jalen@compuamerica.com.ve>
2023-07-19 09:15:06 -0400

    Update api

--- src/db/NinjaPark.js
+++ src/db/NinjaPark.js
@@ -20,20 +20,28 @@ const createUsersFromNinja = async(createUsersFromNinja) => {
         let rowsAffected = 0;
         let pool = await sql.connect(sqlConfig);
         let insert = await pool.request()
-          .input('tipoid', sql.Char, createUsersFromNinja.tipoid)
-          .input('cedula', sql.NVarChar, createUsersFromNinja.cedula)
-          .input('nombApell', sql.NVarChar, createUsersFromNinja.nombApell)
-          .input('fechanac', sql.NVarChar, createUsersFromNinja.fechanac)
-          .input('correo', sql.NVarChar, createUsersFromNinja.correo)
-          .input('nrofac', sql.NVarChar, createUsersFromNinja.nrofac)
-          .input('cantidad_tickes', sql.SmallInt, createUsersFromNinja.cantidad_tickes)
-          .input('localidad', sql.NVarChar, createUsersFromNinja.localidad)
-          .input('plan_adquirido', sql.SmallInt, createUsersFromNinja.plan_adquirido)
-          .input('fecha_in', sql.NVarChar, createUsersFromNinja.fecha_in)
-          .input('fecha_out', sql.NVarChar, createUsersFromNinja.fecha_out)
-          .input('cantidad_personas', sql.NVarChar, createUsersFromNinja.cantidad_personas)
-          .query('insert into np_recibos (tipoid, cedula, nombApell, fechanac, correo, nrofac, cantidad_tickes, localidad, plan_adquirido, fecha_in, fecha_out, cantidad_personas) values (@tipoid, @cedula, @nombApell, @fechanac, @correo, @nrofac, @cantidad_tickes, @localidad, @plan_adquirido, @fecha_in, @fecha_out, @cantidad_personas)')        
-          rowsAffected = rowsAffected + insert.rowsAffected;
+          .input('tipoid', sql.Char, createUsersFromNinja.data.tipoid)
+          .input('cedula', sql.NVarChar, createUsersFromNinja.data.cedula)
+          .input('nombApell', sql.NVarChar, createUsersFromNinja.data.nombApell)
+          .input('fechanac', sql.NVarChar, createUsersFromNinja.data.fechanac)
+          .input('correo', sql.NVarChar, createUsersFromNinja.data.correo)
+          .input('nrofac', sql.NVarChar, createUsersFromNinja.data.nrofac)
+          .input('cantidad_tickes', sql.SmallInt, createUsersFromNinja.data.cantidad_tickes)
+          .input('localidad', sql.NVarChar, createUsersFromNinja.data.localidad)
+          .input('plan_adquirido', sql.SmallInt, createUsersFromNinja.data.plan_adquirido)
+          .input('fecha_in', sql.NVarChar, createUsersFromNinja.data.fecha_in)
+          .input('fecha_out', sql.NVarChar, createUsersFromNinja.data.fecha_out)
+          .query('insert into np_recibos (tipoid, cedula, nombApell, fechanac, correo, nrofac, cantidad_tickes, localidad, plan_adquirido, fecha_in, fecha_out) values (@tipoid, @cedula, @nombApell, @fechanac, @correo, @nrofac, @cantidad_tickes, @localidad, @plan_adquirido, @fecha_in, @fecha_out)')  
+
+          if(insert.rowsAffected > 0){
+            for(let i = 0; i < createUsersFromNinja.acopanantes.length; i++){
+              let insert = await pool.request()
+                .input('nrofac', sql.NVarChar, createUsersFromNinja.data.nrofac)
+                .input('nombre_acompa', sql.NVarChar, createUsersFromNinja.acopanantes[i].nombre_acompa)
+                .input('edad', sql.NVarChar, createUsersFromNinja.acopanantes[i].edad)
+                .query('insert into np_acompanantes (nrofac, nombre_acompa, edad) values (@nrofac, @nombre_acompa, @edad)')  
+            }
+          }
           const createUN = rowsAffected   
           return createUN
     }
