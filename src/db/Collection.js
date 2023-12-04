ae0e2415d085c6c21574d1df48313dc33064e174
Franjhe <franjhely.andre13@gmail.com>
2023-12-04 15:14:34 -0400

    add query payment report

--- src/db/Collection.js
+++ src/db/Collection.js
@@ -44,78 +44,96 @@ const searchDataReceipt = async(searchDataReceipt) => {
 
 const createPaymentReportW = async(createPaymentReport) => {
     try{
-        //inserta en cbreporte pago
-        let pool = await sql.connect(sqlConfig);
-        let insertReport = await pool.request()
-        .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
-        .input('cmoneda'      , sql.Char(4, 0), createPaymentReport.cmoneda )  
-        .input('mpago_dec'        , sql.Numeric(18, 2), createPaymentReport.mpago_dec ) 
-        .input('mpago'        , sql.Numeric(18, 2), createPaymentReport.mpago ) 
-        .input('mpagoext'     , sql.Numeric(18, 2), createPaymentReport.mpagoext)        
-        .input('ptasamon'     , sql.Numeric(18, 2), createPaymentReport.ptasamon )        
-        .input('freporte'     , sql.DateTime , createPaymentReport.freporte )
-        .input('xruta'        , sql.VarChar(1000, 0), createPaymentReport.xruta )  
-        .input('cprog'        , sql.Char(20, 0), createPaymentReport.cprog )
-        .input('cusuario'     , sql.Numeric(18, 0), createPaymentReport.cusuario)  
-        //.input('ccategoria'   , sql.Numeric(18, 0), createPaymentReport.ccategoria)
-        .query('INSERT INTO cbreporte_pago  '
-        +'(casegurado, cmoneda, mpago_dec,  mpago, mpagoext, ptasamon, freporte, xruta, cprog, cusuario )'
-        +'VALUES (@casegurado, @cmoneda, @mpago_dec, @mpago, @mpagoext, @ptasamon, @freporte, @xruta, @cprog, @cusuario )')
-
-
-        //buscamos el numero de caja que acabamos de crear
-        if(insertReport.rowsAffected){
+            //inserta en cbreporte_tran
             let pool = await sql.connect(sqlConfig);
-            let searchBox = await pool.request()
-            .query('SELECT MAX(ncaja) AS ncaja from cbreporte_pago')
-  
-            //Insertamos los recibos que el cliente selecciono para la respectiva validacion
-                if(searchBox.recordset[0].ncaja){
-                    for(let i = 0; i < createPaymentReport.receipt.length; i++){
-                        let pool = await sql.connect(sqlConfig);
-                        let insertReportDet = await pool.request()
-                        .input('ncaja'   , sql.Numeric(18, 0), searchBox.recordset[0].ncaja)
-                        .input('crecibo'   , sql.Numeric(18, 0), createPaymentReport.receipt[i].crecibo)   
-                        .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
-                        .input('cnpoliza'   , sql.Char(30, 0), createPaymentReport.receipt[i].cnpoliza)   
-                        .input('cnrecibo'      , sql.Char(30, 0), createPaymentReport.receipt[i].cnrecibo )  
-                        .input('cpoliza'        , sql.Numeric(19, 0), createPaymentReport.receipt[i].cpoliza ) 
-                        .input('fanopol'        , sql.Numeric(18, 0), createPaymentReport.receipt[i].fanopol ) 
-                        .input('fmespol'     , sql.TinyInt, createPaymentReport.receipt[i].fmespol)        
-                        .input('cramo'     , sql.SmallInt, createPaymentReport.receipt[i].cramo )        
-                        .input('cmoneda'     , sql.Char(4,0) , createPaymentReport.receipt[i].cmoneda )
-                        .input('fdesde_pol'        , sql.DateTime, createPaymentReport.receipt[i].fdesde_pol )  
-                        .input('fhasta_pol'        , sql.DateTime , createPaymentReport.receipt[i].fhasta_pol )
-                        .input('fdesde_rec'     , sql.DateTime, createPaymentReport.receipt[i].fdesde_rec)  
-                        .input('fhasta_rec'   , sql.DateTime, createPaymentReport.receipt[i].fhasta_rec)   
-                        .input('mprimabruta'      , sql.Numeric(4, 2), createPaymentReport.receipt[i].mprimabruta )  
-                        .input('mprimabrutaext'        , sql.Numeric(18, 2), createPaymentReport.receipt[i].mprimabrutaext ) 
-                        .input('ptasamon'        , sql.Numeric(18, 2), createPaymentReport.receipt[i].ptasamon ) 
-                        .input('cusuario'     , sql.Numeric(18, 0), createPaymentReport.cusuario) 
-                        .query('INSERT INTO cbreporte_pago_d  '
-                        +'(ncaja, crecibo, casegurado, cnpoliza, cnrecibo, cpoliza, fanopol, fmespol, cramo, cmoneda, fdesde_pol, fhasta_pol, fdesde_rec, mprimabruta , mprimabrutaext, ptasamon, cusuario)'
-                        +'VALUES (@ncaja, @crecibo, @casegurado, @cnpoliza, @cnrecibo, @cpoliza, @fanopol, @fmespol, @cramo, @cmoneda, @fdesde_pol, @fhasta_pol, @fdesde_rec, @mprimabruta , @mprimabrutaext, @ptasamon, @cusuario )')
+            let inserTransaccion = await pool.request()
+            .input('freporte'     , sql.DateTime , createPaymentReport.freporte )
+            .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
+            .input('mpago'        , sql.Numeric(18, 2), createPaymentReport.mpago ) 
+            .input('mpagoext'     , sql.Numeric(18, 2), createPaymentReport.mpagoext)        
+            .input('ptasamon'     , sql.Numeric(18, 2), createPaymentReport.ptasamon )         
+            .input('cprog'        , sql.Char(20, 0), createPaymentReport.cprog )
+            .input('ifuente'     , sql.Numeric(18, 0), createPaymentReport.ifuente)  
+            .input('cusuario'     , sql.Numeric(18, 0), createPaymentReport.cusuario)  
+            .query('INSERT INTO cbreporte_tran  '
+            +'(freporte, casegurado, mpago,  mpagoext, ptasamon, cprog, ifuente, cusuario )'
+            +'VALUES (@freporte, @casegurado, @mpago, @mpagoext, @ptasamon, @cprog, @ifuente, @cusuario)')
+            //busca el valor dr la transaccion para asignarlo 
+            if(inserTransaccion.rowsAffected > 0 ){
+                let searchTransaccion = await pool.request()
+                .query('SELECT MAX(ctransaccion) AS ctransaccion from cbreporte_pago')
+                //codigo de la transaccion
+                if (searchTransaccion.recordset[0].ctransaccion){
+                    for(let i = 0; i < createPaymentReport.transfer.length; i++){
+                        let insertReport = await pool.request()
+                        .input('ctransaccion'   , sql.Numeric(18, 0), createPaymentReport.transfer.ctransaccion)   
+                        .input('npago'   , sql.Numeric(18, 0), i + 1)   
+                        .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.transfer.casegurado)   
+                        .input('cmoneda'      , sql.Char(4, 0), createPaymentReport.transfer.cmoneda )  
+                        .input('cbanco'        , sql.Numeric(18, 2), createPaymentReport.transfer.cbanco ) 
+                        .input('cbanco_destino'        , sql.Numeric(18, 2), createPaymentReport.cbanco_destino ) 
+                        .input('mpago'        , sql.Numeric(18, 2), createPaymentReport.transfer.mpago ) 
+                        .input('mpagoext'     , sql.Numeric(18, 2), createPaymentReport.transfer.mpagoext)        
+                        .input('ptasamon'     , sql.Numeric(18, 2), createPaymentReport.transfer.ptasamon )        
+                        .input('ptasaref'     , sql.Numeric(18, 2), createPaymentReport.transfer.ptasaref )        
+                        .input('freporte'     , sql.DateTime , createPaymentReport.transfer.freporte )
+                        .input('xreferencia'  , sql.VarChar(100, 0), createPaymentReport.transfer.xreferencia )  
+                        .input('xruta'        , sql.VarChar(100, 0), createPaymentReport.transfer.xruta )  
+                        .input('cprog'        , sql.Char(20, 0), createPaymentReport.cprog )
+                        .input('cusuario'     , sql.Numeric(18, 0), createPaymentReport.cusuario)  
+                        .query('INSERT INTO cbreporte_pago  '
+                        +'(ctransaccion, npago,  casegurado, cmoneda, cbanco, cbanco_destino, mpago, mpagoext, ptasamon, ptasaref, freporte, xreferencia, xruta, cprog, cusuario ) VALUES'
+                        +'(@ctransaccion, @npago, @casegurado, @cmoneda, @cbanco, @cbanco_destino, @mpago, @mpagoext, @ptasamon, @ptasaref, @freporte, @xreferencia, @xruta, @cprog, @cusuario )')
 
-                        //actualizasmos el estado del recibo
-                        if(insertReportDet.rowsAffected){
-                            try{
-                                for(let i = 0; i < createPaymentReport.receipt.length; i++){
-                                    let pool = await sql.connect(sqlConfig);
-                                    let updateReceipt= await pool.request()
-                                    .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
-                                    .input('cnpoliza'   , sql.Char(30, 0), createPaymentReport.receipt[i].cnpoliza)   
-                                    .input('cnrecibo'      , sql.Char(30, 0), createPaymentReport.receipt[i].cnrecibo )  
-                                    .input('iestadorec'     , sql.Char(1, 0),  createPaymentReport.iestadorec) 
-                                    .query('update adrecibos set iestadorec = @iestadorec where casegurado = @casegurado and cnpoliza = @cnpoliza and cnrecibo = @cnrecibo' );
+                        if(insertReport.rowsAffected > 0){
+                            for(let i = 0; i < createPaymentReport.receipt.length; i++){
+                                //insertamos detalle nde los recibos pagados 
+                                let pool = await sql.connect(sqlConfig);
+                                let insertReportDet = await pool.request()
+                                .input('ctransaccion'   , sql.Numeric(18, 0), createPaymentReport.transfer.ctransaccion)   
+                                .input('crecibo'   , sql.Numeric(18, 0), createPaymentReport.receipt[i].crecibo)   
+                                .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
+                                .input('cnpoliza'   , sql.Char(30, 0), createPaymentReport.receipt[i].cnpoliza)   
+                                .input('cnrecibo'      , sql.Char(30, 0), createPaymentReport.receipt[i].cnrecibo )  
+                                .input('cpoliza'        , sql.Numeric(19, 0), createPaymentReport.receipt[i].cpoliza ) 
+                                .input('fanopol'        , sql.Numeric(18, 0), createPaymentReport.receipt[i].fanopol ) 
+                                .input('fmespol'     , sql.TinyInt, createPaymentReport.receipt[i].fmespol)        
+                                .input('cramo'     , sql.SmallInt, createPaymentReport.receipt[i].cramo )        
+                                .input('cmoneda'     , sql.Char(4,0) , createPaymentReport.receipt[i].cmoneda )
+                                .input('fdesde_pol'        , sql.DateTime, createPaymentReport.receipt[i].fdesde_pol )  
+                                .input('fhasta_pol'        , sql.DateTime , createPaymentReport.receipt[i].fhasta_pol )
+                                .input('fdesde_rec'     , sql.DateTime, createPaymentReport.receipt[i].fdesde_rec)  
+                                .input('fhasta_rec'   , sql.DateTime, createPaymentReport.receipt[i].fhasta_rec)   
+                                .input('mprimabruta'      , sql.Numeric(4, 2), createPaymentReport.receipt[i].mprimabruta )  
+                                .input('mprimabrutaext'        , sql.Numeric(18, 2), createPaymentReport.receipt[i].mprimabrutaext ) 
+                                .input('ptasamon'        , sql.Numeric(18, 2), createPaymentReport.receipt[i].ptasamon ) 
+                                .input('cusuario'     , sql.Numeric(18, 0), createPaymentReport.cusuario) 
+                                .query('INSERT INTO cbreporte_pago_d  '
+                                +'(ctransaccion, crecibo, casegurado, cnpoliza, cnrecibo, cpoliza, fanopol, fmespol, cramo, cmoneda, fdesde_pol, fhasta_pol, fdesde_rec, mprimabruta , mprimabrutaext, ptasamon, cusuario)'
+                                +'VALUES (@ctransaccion, @crecibo, @casegurado, @cnpoliza, @cnrecibo, @cpoliza, @fanopol, @fmespol, @cramo, @cmoneda, @fdesde_pol, @fhasta_pol, @fdesde_rec, @mprimabruta , @mprimabrutaext, @ptasamon, @cusuario )')
+        
+                                //actualizasmos el estado del recibo
+                                if(insertReportDet.rowsAffected){
+                                    try{
+                                        for(let i = 0; i < createPaymentReport.receipt.length; i++){
+                                            let pool = await sql.connect(sqlConfig);
+                                            let updateReceipt= await pool.request()
+                                            .input('casegurado'   , sql.Numeric(18, 0), createPaymentReport.casegurado)   
+                                            .input('cnpoliza'   , sql.Char(30, 0), createPaymentReport.receipt[i].cnpoliza)   
+                                            .input('cnrecibo'      , sql.Char(30, 0), createPaymentReport.receipt[i].cnrecibo )  
+                                            .input('iestadorec'     , sql.Char(1, 0),  createPaymentReport.iestadorec) 
+                                            .query('update adrecibos set iestadorec = @iestadorec where casegurado = @casegurado and cnpoliza = @cnpoliza and cnrecibo = @cnrecibo' );
+                                        }
+                                    }catch(err){
+                                        return { error: err.message, message: 'No se registraron los datos ' };
+                                    }
                                 }
-                            }catch(err){
-                                return { error: err.message, message: 'No se registrarons los datos ' };
                             }
                         }
                     }
                 }
-        }
-
+            }
+    
         await pool.close();
         return { result: insertReport };
 
