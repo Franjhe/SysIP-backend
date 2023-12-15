f5e2cbed60a6311556a4da7f21252894c085139b
JhonAlenH <alenjhon9@gmail.com>
2023-12-15 12:34:00 -0400

    autos

--- src/db/Quotes.js
+++ src/db/Quotes.js
@@ -12,18 +12,18 @@ const sqlConfig = {
 }
 
 const createQuotes = async (createQuotes) => {
-    console.log(createQuotes)
     try {
         let pool = await sql.connect(sqlConfig);
         let result = await pool.request()
             .input('id_inma', sql.Int, createQuotes.id_inma)
             .input('fano', sql.Int, createQuotes.fano)
-            .input('xcobertura', sql.NVarChar, createQuotes.xcobertura)
             .input('xnombre', sql.NVarChar, createQuotes.xnombre)
             .input('xapellido', sql.NVarChar, createQuotes.xapellido)
             .input('xcorreo', sql.NVarChar, createQuotes.email)
             .input('ctarifa_exceso', sql.Int, createQuotes.ctarifa_exceso)
-            .input('cuso', sql.Int, createQuotes.cuso)
+            .input('msuma', sql.Numeric(18, 2), createQuotes.msuma_aseg)
+            .input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion)
+            .input('ncapacidad_p', sql.NVarChar, createQuotes.ncapacidad_p)
             .execute('trBCotizacionAuto');
 
         let query = await pool.request()
@@ -38,15 +38,17 @@ const createQuotes = async (createQuotes) => {
 }
 
 const updateQuotes = async (updateQuotes) => {
-
     try {
         let pool = await sql.connect(sqlConfig);
         let update = await pool.request()
             .input('ccotizacion', sql.Int, updateQuotes.ccotizacion)
             .input('cplan_rc', sql.Int, updateQuotes.cplan_rc)
+            .input('brcv', sql.Bit, updateQuotes.brcv)
+            .input('bamplia', sql.Bit, updateQuotes.bamplia)
+            .input('bperdida', sql.Bit, updateQuotes.bperdida)
             .input('iaceptado', sql.Bit, updateQuotes.iaceptado)
             .input('faceptado', sql.DateTime, new Date())
-            .query('UPDATE TMEMISION_COTIZACION SET iaceptado = @iaceptado, faceptado = @faceptado WHERE ccotizacion = @ccotizacion AND cplan_rc = @cplan_rc')
+            .query('UPDATE TMEMISION_COTIZACION SET iaceptado = @iaceptado, brcv = @brcv, bamplia = @bamplia, bperdida = @bperdida, faceptado = @faceptado WHERE ccotizacion = @ccotizacion AND cplan_rc = @cplan_rc')
 
         return update;
     } catch (error) {
