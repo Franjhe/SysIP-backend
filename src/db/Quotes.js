70aa1b77c39b38e98e4f7e60c4b28e84ad0c14b9
unknown <jalen@compuamerica.com.ve>
2023-12-20 20:15:12 -0400

    quotes

--- src/db/Quotes.js
+++ src/db/Quotes.js
@@ -12,19 +12,19 @@ const sqlConfig = {
 }
 
 const createQuotes = async (createQuotes) => {
-    console.log(createQuotes)
     try {
         let pool = await sql.connect(sqlConfig);
         let result = await pool.request()
             .input('id_inma', sql.Int, createQuotes.id_inma)
-            .input('fano', sql.Int, createQuotes.fano)
             .input('xnombre', sql.NVarChar, createQuotes.xnombre)
             .input('xapellido', sql.NVarChar, createQuotes.xapellido)
-            .input('xcorreo', sql.NVarChar, createQuotes.email)
             .input('ctarifa_exceso', sql.Int, createQuotes.ctarifa_exceso)
-            .input('msuma', sql.Numeric(18, 2), createQuotes.msuma_aseg)
-            .input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion.trim())
+            .input('xcorreo', sql.NVarChar, createQuotes.email)
+            .input('fano', sql.Int, createQuotes.fano)
             .input('ncapacidad_p', sql.Int, createQuotes.ncapacidad_p)
+            .input('msuma', sql.Numeric(18, 2), createQuotes.msum)
+            .input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion.trim())
+            
             .execute('trBCotizacionAuto');
 
         let query = await pool.request()
