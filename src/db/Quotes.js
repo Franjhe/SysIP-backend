fd7189384cfe7c408a787b53c556bad9ace1ce2a
unknown <jalen@compuamerica.com.ve>
2024-03-04 14:38:39 -0400

    quotes

--- src/db/Quotes.js
+++ src/db/Quotes.js
@@ -19,10 +19,52 @@ const Detail = sequelize.define('TMEMISION_COTIZACION', {}, { tableName: 'TMEMIS
 const DetailAuto = sequelize.define('VWBUSCARCOTIZACION', {}, { tableName: 'VWBUSCARCOTIZACION' });
 const Search = sequelize.define('VWBUSCARCOTIZACION', {}, { tableName: 'VWBUSCARCOTIZACION' });
 
+// const createQuotes = async (createQuotes) => {
+//     console.log(createQuotes)
+//     try {
+//         let pool = await sql.connect(sqlConfig);
+//         let request = pool.request()
+//             .input('id_inma', sql.Int, createQuotes.id_inma)
+//             .input('xnombre', sql.NVarChar, createQuotes.xnombre)
+//             .input('xapellido', sql.NVarChar, createQuotes.xapellido)
+//             .input('ctarifa_exceso', sql.Int, createQuotes.ctarifa_exceso)
+//             .input('xcorreo', sql.NVarChar, createQuotes.email)
+//             .input('fano', sql.Int, createQuotes.fano)
+//             .input('ncapacidad_p', sql.Int, createQuotes.ncapacidad_p)
+//             .input('msuma', sql.Numeric(18, 2), createQuotes.msum)
+//             .input('ccorredor', sql.Int, createQuotes.ccorredor)
+//             .input('xtipo', sql.Char, createQuotes.xtipo);
+
+//         if (createQuotes.xclasificacion) {
+//             request.input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion.trim());
+//         }else{
+//             request.input('xclasificacion', sql.NVarChar, null);
+//         }
+
+//         let result = await request.execute('trBCotizacionAuto');
+
+//         if(createQuotes.xtipo == 'V'){
+//             let query = await pool.request()
+//             .query('SELECT TOP 5 ccotizacion, xmarca, xmodelo, xnombre, xapellido, cplan_rc, xplan_rc, mtotal_rcv, mtotal_amplia, mtotal_perdida, xcorredor, xcorreocorredor, xtelefonocorredor, pperdida_total, pcobertura_amplia FROM VWBUSCARCOTIZACION ORDER BY ccotizacion DESC');
+//         }else{
+//             let query = await pool.request()
+//             .query('SELECT TOP 1 ccotizacion, xmarca, xmodelo, xnombre, xapellido, cplan_rc, xplan_rc, mtotal_rcv, mtotal_amplia, mtotal_perdida, xcorredor, xcorreocorredor, xtelefonocorredor, pperdida_total, pcobertura_amplia FROM VWBUSCARCOTIZACION ORDER BY ccotizacion DESC');
+//         }
+        
+//         await pool.close();
+//         return { result: query.recordset };
+
+//     } catch (err) {
+//         console.log(err.message);
+//         return { error: err.message };
+//     }
+// }
+
 const createQuotes = async (createQuotes) => {
+    console.log(createQuotes);
     try {
         let pool = await sql.connect(sqlConfig);
-        let result = await pool.request()
+        let request = pool.request()
             .input('id_inma', sql.Int, createQuotes.id_inma)
             .input('xnombre', sql.NVarChar, createQuotes.xnombre)
             .input('xapellido', sql.NVarChar, createQuotes.xapellido)
@@ -31,13 +73,26 @@ const createQuotes = async (createQuotes) => {
             .input('fano', sql.Int, createQuotes.fano)
             .input('ncapacidad_p', sql.Int, createQuotes.ncapacidad_p)
             .input('msuma', sql.Numeric(18, 2), createQuotes.msum)
-            .input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion.trim())
             .input('ccorredor', sql.Int, createQuotes.ccorredor)
-            
-            .execute('trBCotizacionAuto');
+            .input('xtipo', sql.Char, createQuotes.xtipo);
 
-        let query = await pool.request()
-            .query('SELECT TOP 5 ccotizacion, xmarca, xmodelo, xnombre, xapellido, cplan_rc, xplan_rc, mtotal_rcv, mtotal_amplia, mtotal_perdida, xcorredor, xcorreocorredor, xtelefonocorredor, pperdida_total, pcobertura_amplia FROM VWBUSCARCOTIZACION ORDER BY ccotizacion DESC');
+        if (createQuotes.xclasificacion) {
+            request.input('xclasificacion', sql.NVarChar, createQuotes.xclasificacion.trim());
+        } else {
+            request.input('xclasificacion', sql.NVarChar, null);
+        }
+
+        let result = await request.execute('trBCotizacionAuto');
+        let query;
+
+        if (createQuotes.xtipo == 'V') {
+            query = await pool.request()
+                .query('SELECT TOP 5 ccotizacion, xmarca, xmodelo, xnombre, xapellido, cplan_rc, xplan_rc, mtotal_rcv, mtotal_amplia, mtotal_perdida, xcorredor, xcorreocorredor, xtelefonocorredor, pperdida_total, pcobertura_amplia FROM VWBUSCARCOTIZACION ORDER BY ccotizacion DESC');
+        } else {
+            query = await pool.request()
+                .query('SELECT TOP 1 ccotizacion, xmarca, xmodelo, xnombre, xapellido, cplan_rc, xplan_rc, mtotal_rcv, mtotal_amplia, mtotal_perdida, xcorredor, xcorreocorredor, xtelefonocorredor, pperdida_total, pcobertura_amplia FROM VWBUSCARCOTIZACION ORDER BY ccotizacion DESC');
+        }
+
         await pool.close();
         return { result: query.recordset };
 
@@ -213,26 +268,64 @@ const detailQuotesAutomobile = async (detailQuotesAutomobile) => {
     try {
         let pool = await sql.connect(sqlConfig);
         console.log(updatePremiums.quotes)
-        for (let i = 0; i < updatePremiums.quotes.length; i++) {
-            let update = await pool.request()
-                .input('ccotizacion', sql.Int, updatePremiums.ccotizacion)
-                .input('msuma_amplia', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
-                .input('msuma_total', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
-                .input('msuma_catastrofico', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
-                .input('msuma_motin', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
-                .input('mtotal_amplia', sql.Numeric(18, 2), updatePremiums.quotes[i].mtotal_amplia)
-                .input('mtotal_perdida', sql.Numeric(18, 2), updatePremiums.quotes[i].mtotal_perdida)
-                .input('cplan_rc', sql.Numeric(18, 2), updatePremiums.quotes[i].cplan_rc)
-                .query('UPDATE TMEMISION_COTIZACION SET msuma_amplia = @msuma_amplia, msuma_total = @msuma_total, msuma_catastrofico = @msuma_catastrofico, msuma_motin = @msuma_motin, mtotal_amplia = @mtotal_amplia, mtotal_perdida = @mtotal_perdida WHERE ccotizacion = @ccotizacion and cplan_rc = @cplan_rc');
-        }
+        if (updatePremiums.quotes) {
+            for (let i = 0; i < updatePremiums.quotes.length; i++) {
+                let mtotal_amplia = isNaN(updatePremiums.quotes[i].mtotal_amplia) ? 0 : updatePremiums.quotes[i].mtotal_amplia;
+                let mtotal_perdida = isNaN(updatePremiums.quotes[i].mtotal_perdida) ? 0 : updatePremiums.quotes[i].mtotal_perdida;
+                let cplan_rc = isNaN(updatePremiums.quotes[i].cplan_rc) ? 0 : updatePremiums.quotes[i].cplan_rc;
 
+                if (updatePremiums.quotes[i].pcobertura_amplia == null) {
+                    let msuma_amplia = 0
+                    let update = await pool.request()
+                        .input('ccotizacion', sql.Int, updatePremiums.ccotizacion)
+                        .input('msuma_amplia', sql.Numeric(18, 2), msuma_amplia)
+                        .input('msuma_total', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_catastrofico', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_motin', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('mtotal_amplia', sql.Numeric(18, 2), mtotal_amplia)
+                        .input('mtotal_perdida', sql.Numeric(18, 2), mtotal_perdida)
+                        .input('cplan_rc', sql.Numeric(18, 2), cplan_rc)
+                        .query('UPDATE TMEMISION_COTIZACION SET msuma_amplia = @msuma_amplia, msuma_total = @msuma_total, msuma_catastrofico = @msuma_catastrofico, msuma_motin = @msuma_motin, mtotal_amplia = @mtotal_amplia, mtotal_perdida = @mtotal_perdida WHERE ccotizacion = @ccotizacion and cplan_rc = @cplan_rc');
+
+                        if(msuma_amplia){
+                            let updateReporte = await pool.request()
+                            .query('UPDATE tmreporte_cotiza SET m2 = NULL');
+                        }
+
+                } else if (updatePremiums.quotes[i].pperdida_total == null) {
+                    let msuma_total = 0
+                    let update = await pool.request()
+                        .input('ccotizacion', sql.Int, updatePremiums.ccotizacion)
+                        .input('msuma_amplia', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_total', sql.Numeric(18, 2), msuma_total)
+                        .input('msuma_catastrofico', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_motin', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('mtotal_amplia', sql.Numeric(18, 2), mtotal_amplia)
+                        .input('mtotal_perdida', sql.Numeric(18, 2), mtotal_perdida)
+                        .input('cplan_rc', sql.Numeric(18, 2), cplan_rc)
+                        .query('UPDATE TMEMISION_COTIZACION SET msuma_amplia = @msuma_amplia, msuma_total = @msuma_total, msuma_catastrofico = @msuma_catastrofico, msuma_motin = @msuma_motin, mtotal_amplia = @mtotal_amplia, mtotal_perdida = @mtotal_perdida WHERE ccotizacion = @ccotizacion and cplan_rc = @cplan_rc');
+
+                    let updateReporte = await pool.request()
+                        .query('UPDATE tmreporte_cotiza SET m3 = NULL');
+                } else {
+                    let update = await pool.request()
+                        .input('ccotizacion', sql.Int, updatePremiums.ccotizacion)
+                        .input('msuma_amplia', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_total', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_catastrofico', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('msuma_motin', sql.Numeric(18, 2), updatePremiums.msuma_aseg)
+                        .input('mtotal_amplia', sql.Numeric(18, 2), mtotal_amplia)
+                        .input('mtotal_perdida', sql.Numeric(18, 2), mtotal_perdida)
+                        .input('cplan_rc', sql.Numeric(18, 2), cplan_rc)
+                        .query('UPDATE TMEMISION_COTIZACION SET msuma_amplia = @msuma_amplia, msuma_total = @msuma_total, msuma_catastrofico = @msuma_catastrofico, msuma_motin = @msuma_motin, mtotal_amplia = @mtotal_amplia, mtotal_perdida = @mtotal_perdida WHERE ccotizacion = @ccotizacion and cplan_rc = @cplan_rc');
+                }
+            }
+        }
         return { success: true, message: 'Cotizaciones actualizadas correctamente' };
     } catch (error) {
-        console.log(error);
         return { success: false, message: 'Error al actualizar las Cotizaciones', error };
     }
 };
-
 
 export default {
     createQuotes,
