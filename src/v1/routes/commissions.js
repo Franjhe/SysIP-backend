8ca5133c31b4e3464624c527520b82cd2887df04
Gabriel Estacio <gestacio@lamundialdeseguros.com>
2024-03-06 15:47:49 -0400

    al menos lee algo

---
+++ src/v1/routes/commissions.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import commissionsController from '../../controllers/commissionsController.js'
+
+import fileExtension from 'file-extension';
+import multer from 'multer';
+
+
+const DOCUMENTS_PATH = './public/documents'; 
+
+const document_storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, DOCUMENTS_PATH);
+    },
+  
+    filename: (req, file, cb) => {
+        console.log(file, req)
+      cb(null, file.fieldname + '-' + Date.now() + '.' + fileExtension(file.originalname));
+    }
+});
+  
+let document_upload = multer({
+    storage: document_storage,
+    limits: {
+    fileSize: 5000000
+    },
+    fileFilter(req, file, cb) {
+    cb(null, true);
+    }
+    
+});
+
+const router = express.Router();
+
+router
+
+    .post("/",  commissionsController.searchCualquierData)
+    .post("/search",  commissionsController.searchCommissionsbyClient)
+    .post("/create-trans",  commissionsController.createPaymentReporTrans) //creacion del movimiento de transaccion del reporte de pago
+    .post("/create-report",  commissionsController.createPaymentReportSoport) //abjuncion de las imagenes para el transaccion del reporte de pago
+    
+    
+    //api de carga de transacciones 
+    
+    // .post("/create-notification-movement", document_upload.single('file'), commissionsController.createNotificationMovement   ) //creacion del movimiento de transaccion del reporte de pago
+
+
+    
+    .get("/search-notification",  commissionsController.searchPaymentReportNotification)
+    .get("/search-pending",  commissionsController.searchPaymentPending)
+    .get("/search-payments-collected",  commissionsController.PaymentsCollected)
+    .get("/search-vencido",  commissionsController.searchPaymentVencin)
+    .get("/search-collected",  commissionsController.searchPaymentCollected)
+
+    .post("/receipt-under-review",  commissionsController.receiptUnderReview) //abjuncion de las imagenes para el transaccion del reporte de pago
+    .get("/search-difference-of-notification",  commissionsController.differenceOfNotification) //abjuncion de las imagenes para el transaccion del reporte de pago
+    .patch("/update-difference-of-notification",  commissionsController.updateDifferenceOfNotification) 
+
+
+    //busquedas de notificaciones
+    .get("/search-notification-data/:id",  commissionsController.searchPaymentReportNotificationData)
+    .patch("/update-receipt/",  commissionsController.updateReceipt)
+    .get("/search-receipt-data/:id",  commissionsController.searchReceiptClient)
+    .get("/search-client/:id",  commissionsController.searchClient)
+
+
+
+
+export default router;
