1d67e446f49ca0c1e4d877fbfa1ebd04d736029f
Franjhe <franjhely.andre13@gmail.com>
2024-03-15 13:38:27 -0400

    cors

--- src/server.js
+++ src/server.js
@@ -120,7 +120,7 @@ app.post('/api/upload/documents', document_upload.array('xdocumentos', 5), (req,
   res.json({ data: { status: true, uploadedFile: files } });
 });
 
-app.post('/api/upload/image', document_upload.single('file'), cors(),(req, res , err) => {
+app.post('/api/upload/image', document_upload.single('file'),(req, res , err) => {
   const files = req.file;
   if (!files || files.length === 0) {
     const error = new Error('Please upload at least one file');
