bc907c5378bf13b714e0c615b17ed8ae96bd9f24
Jhon Alen <jalen@lamundialdeseguros.com>
2024-03-20 12:58:29 -0400

    certificated

--- src/service/certificateService.js
+++ src/service/certificateService.js
@@ -149,14 +149,23 @@ const detailCertificateCertificate = async (searchDetail) => {
         let xestado_tomador; let xciudad_tomador;
         if(getTakers.error){ return { status: false, code: 500, message: getTakers.error }; }
         if(getTakers.result.rowsAffected > 0){ 
-            xtomador = getTakers.result.recordset[0].XTOMADOR
-            xrif_tomador = getTakers.result.recordset[0].XRIF
+            // xtomador = getTakers.result.recordset[0].XTOMADOR
             xdireccion_tomador = getTakers.result.recordset[0].XDIRECCION
             xzona_postal_tomador = getTakers.result.recordset[0].XZONA_POSTAL
             xtelefono_tomador = getTakers.result.recordset[0].XTELEFONO
             xcorreo_tomador = getTakers.result.recordset[0].XCORREO
             xestado_tomador = getTakers.result.recordset[0].XESTADO
             xciudad_tomador = getTakers.result.recordset[0].XCIUDAD
+
+            if(getTakers.result.recordset[0].XTOMADOR){
+                xtomador = getTakers.result.recordset[0].XTOMADOR.toUpperCase();
+            }
+
+            if(getTakers.result.recordset[0].XRIF){
+                xrif_tomador = getFleetContractOwnerData.result.recordset[0].ICEDULA + '-' + getTakers.result.recordset[0].XRIF
+            }else{
+                xrif_tomador = null
+            }
         }
         return {
             status: true,
@@ -239,8 +248,8 @@ const detailCertificateCertificate = async (searchDetail) => {
             xuso: getFleetContractData.result.recordset[0].XUSO,
             xplancoberturas: getPlanData.result.recordset[0].XPLAN_RC,
             xgrua: getPlanData.result.recordset[0].XGRUA,
-            xtomador: xtomador.toUpperCase(),
-            xrif_tomador: getFleetContractOwnerData.result.recordset[0].ICEDULA + '-' + xrif_tomador,
+            xtomador: xtomador,
+            xrif_tomador: xrif_tomador,
             xdireccion_tomador: xdireccion_tomador,
             xzona_postal_tomador: xzona_postal_tomador,
             xtelefono_tomador: xtelefono_tomador,
